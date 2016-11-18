068d0ee851825d1eef95ac302f81fa45ab1f47c2
abakarEgrey <egreabakar@yahoo.fr>
2016-11-18 18:15:28 +0100

    Removed defaut Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+    
+    @base_title =  "Simple App du Tutoriel Ruby on Rails"
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +17,7 @@ describe PagesController do
 	it "doit avoir le bon titre" do
 	  get 'home'
 	  response.should have_selector("title",
-						:content => "Simple App du Tutoriel Ruby on Rails | Accueil")
+						:content => @base_title + " | Accueil")
 	end
   end
 
@@ -26,7 +31,7 @@ describe PagesController do
       get 'contact'
       response.should have_selector("title",
                         :content =>
-                          "Simple App du Tutoriel Ruby on Rails | Contact")
+                          @base_title + " | Contact")
     end
   end
 
@@ -40,7 +45,21 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                         :content =>
-                          "Simple App du Tutoriel Ruby on Rails | À Propos")
+                          @base_title + " | À Propos")
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "devrait réussir" do
+      get 'help'
+      response.should be_success
+    end
+	
+	it "devrait avoir le bon titre" do
+      get 'help'
+      response.should have_selector("title",
+                        :content =>
+                          @base_title + " | Aide")
     end
   end
 end
